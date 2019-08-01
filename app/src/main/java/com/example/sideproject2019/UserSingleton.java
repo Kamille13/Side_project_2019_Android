13cdcf4aea76f3820cced1763119e098d4d91bb7
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-08-01 16:13:59 +0200

    SignUp activity and UserSingleton

---
+++ app/src/main/java/com/example/sideproject2019/UserSingleton.java
@@ -0,0 +1,27 @@
+package com.example.sideproject2019;
+
+import com.example.sideproject2019.model.User;
+
+public class UserSingleton {
+
+    private static UserSingleton instance;
+    private User user;
+
+    private UserSingleton() {
+    }
+
+    public static UserSingleton getInstance() {
+        if (instance == null) {
+            instance = new UserSingleton();
+        }
+        return instance;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
--- app/src/main/java/com/example/sideproject2019/activity/SignInActivity.java
+++ app/src/main/java/com/example/sideproject2019/activity/SignInActivity.java
@@ -1,9 +1,18 @@
 package com.example.sideproject2019.activity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v4.util.Consumer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.sideproject2019.R;
+import com.example.sideproject2019.SingletonVolley;
+import com.example.sideproject2019.UserSingleton;
+import com.example.sideproject2019.model.User;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -11,5 +20,42 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signIn);
+
+        final EditText etEmail = findViewById(R.id.etEmailSignIn);
+        final EditText etPassword = findViewById(R.id.etPassWordSignIn);
+        Button btSignIn = findViewById(R.id.btSignIn);
+        Button btSignUp = findViewById(R.id.btSignUpSignIn);
+
+        btSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                if (email.isEmpty() || password.isEmpty()) {
+                    new AlertDialog.Builder(SignInActivity.this)
+                            .setTitle("Erreur")
+                            .setMessage("Veuillez renseigner votre email et mot de passe")
+                            .show();
+                }
+                User user = new User();
+                user.setEmail(email);
+                user.setPassword(password);
+
+                SingletonVolley.getInstance(SignInActivity.this)
+                        .SignIn(user, new Consumer<User>() {
+                            @Override
+                            public void accept(User user) {
+                                UserSingleton.getInstance().setUser(user);
+                                if (user != null) {
+                                    Intent intent = new Intent(SignInActivity.this,
+                                            SignUpActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+
+                        });
+            }
+        });
+
     }
 }
