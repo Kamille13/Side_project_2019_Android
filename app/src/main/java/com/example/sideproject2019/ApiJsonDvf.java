fe4e257b40f70324f6481ceb4d79597a7d239274
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-08-29 13:33:25 +0200

    extractApiDVF

--- app/src/main/java/com/example/sideproject2019/ApiJsonDvf.java
+++ app/src/main/java/com/example/sideproject2019/ApiJsonDvf.java
@@ -1,4 +1,74 @@
 package com.example.sideproject2019;
 
+import android.content.Context;
+import android.location.Location;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.sideproject2019.model.Dvf;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class ApiJsonDvf {
+
+    public static void extractAPI(Context context, final Location locationUser, Boolean dropoff, int zoom, final Dvf.DvfListener listener) {
+        String json = null;
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        String url = "http://api.cquest.org/dvf?code_postal=31000";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray resultats = response.getJSONArray("resultats");
+                            ArrayList<Dvf> dvfs = new ArrayList<>();
+                            for (int i = 0; i < resultats.length(); i++) {
+                                JSONObject rec = (JSONObject) resultats.get(i);
+                                long lat = 0;
+                                if (rec.has("lat")) {
+                                    lat = (long) rec.get("lat");
+                                }
+                                long lon = 0;
+                                if (rec.has("lon")) {
+                                    lon = (long) rec.get("lon");
+                                }
+                                String valeur_fonciere = (String) rec.get("valeur_fonciere");
+                                String numero_plan = (String) rec.get("numero_plan");
+                                String type_local = (String) rec.get("type_local");
+                                String surface_relle_bati = (String) rec.get("surface_relle_bati");
+                                String nombre_pieces_principales = (String) rec.get("nombre_pieces_principales");
+
+                                Dvf dvf = new Dvf(lat, lon, valeur_fonciere, numero_plan, type_local, surface_relle_bati, nombre_pieces_principales);
+                                dvfs.add(dvf);
+                            }
+                            listener.onResult(dvfs);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Afficher l'erreur
+                        Log.d("VOLLEY_ERROR", "onErrorResponse: " + error.getMessage());
+                    }
+                }
+        );
+        // On ajoute la requête à la file d'attente
+        requestQueue.add(jsonObjectRequest);
+    }
 }
---
+++ app/src/main/java/com/example/sideproject2019/model/Dvf.java
@@ -0,0 +1,87 @@
+package com.example.sideproject2019.model;
+
+import java.util.ArrayList;
+
+public class Dvf {
+
+    private long  lat;
+    private long lon;
+    private String valeur_fonciere;
+    private String numero_plan;
+    private String type_local;
+    private String surface_relle_bati;
+    private String nombre_pieces_principales;
+
+    public Dvf() {
+    }
+
+    public Dvf(long lat, long lon, String valeur_fonciere, String numero_plan, String type_local, String surface_relle_bati, String nombre_pieces_principales) {
+        this.lat = lat;
+        this.lon = lon;
+        this.valeur_fonciere = valeur_fonciere;
+        this.numero_plan = numero_plan;
+        this.type_local = type_local;
+        this.surface_relle_bati = surface_relle_bati;
+        this.nombre_pieces_principales = nombre_pieces_principales;
+    }
+
+    public long getLat() {
+        return lat;
+    }
+
+    public void setLat(long lat) {
+        this.lat = lat;
+    }
+
+    public long getLon() {
+        return lon;
+    }
+
+    public void setLon(long lon) {
+        this.lon = lon;
+    }
+
+    public String getValeur_fonciere() {
+        return valeur_fonciere;
+    }
+
+    public void setValeur_fonciere(String valeur_fonciere) {
+        this.valeur_fonciere = valeur_fonciere;
+    }
+
+    public String getNumero_plan() {
+        return numero_plan;
+    }
+
+    public void setNumero_plan(String numero_plan) {
+        this.numero_plan = numero_plan;
+    }
+
+    public String getType_local() {
+        return type_local;
+    }
+
+    public void setType_local(String type_local) {
+        this.type_local = type_local;
+    }
+
+    public String getSurface_relle_bati() {
+        return surface_relle_bati;
+    }
+
+    public void setSurface_relle_bati(String surface_relle_bati) {
+        this.surface_relle_bati = surface_relle_bati;
+    }
+
+    public String getNombre_pieces_principales() {
+        return nombre_pieces_principales;
+    }
+
+    public void setNombre_pieces_principales(String nombre_pieces_principales) {
+        this.nombre_pieces_principales = nombre_pieces_principales;
+    }
+
+    public interface DvfListener {
+        void onResult(ArrayList<Dvf> dvfs);
+    }
+}
